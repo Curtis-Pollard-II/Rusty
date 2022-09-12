fc702462446c50b3559c617fa6234472d9c65ace
Curtis Pollard <clp.the.second@gmail.com>
2022-09-12 15:58:57 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+bool playing = true;
+List<string> possibleInputs = new List<string> {"rock", "paper", "scissors"};
+
+while (playing)
+{
+Console.WriteLine("Welcome");
+Console.WriteLine("Input one of of the following words: rock , paper , or scissors");
+
+
+
+}
+string? userInput = "";
+userInput = Console.ReadLine();
+
+// bool validInput = false;
+// if (userInput = possibleInputs)
+//         {validInput == true;}
+//             else { validInput == false; }
+
+
+
+
